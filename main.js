d1efe214ed50843a08e6e0f6bee09cef74b9fa8b
Nick Wilson <logikfx@gmail.com>
2018-02-27 19:49:11 -0500

    Added an object containing several html5 colors

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 /* main.js */
 
-
+var colors = {
+    1: 'Red',
+    2: 'Blue',
+    3: 'Green',
+    4: 'Yellow',
+    5: 'Orange',
+    6: 'DarkViolet',
+    7: 'Grey',
+    8: 'LightBlue',
+    9: 'SkyBlue',
+    10: 'Silver'
+}
 
 // grab context
 var canvas = document.querySelector("#canvas");
 var context = canvas.getContext("2d");
 
-var radius = 10;
+var radius = 5;
 
 var drawParticle = function (context, x, y) {
-    //r = 0;
-    //g = 0;
-    //b = 0;
-    //a = 1;
-    //context.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
     context.fillStyle = 'blue';
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
